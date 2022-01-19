c53bdae407717c58a6b58b3676146d89e9f81e34
bishanmeghani <bishan_rulez@hotmail.com>
2022-01-19 08:22:51 +0000

    positions starts at 1 not 0

--- tasks.py
+++ tasks.py
@@ -1,12 +1,15 @@
 sentence = "ASK NOT WHAT YOUR COUNTRY CAN DO FOR YOU ASK WHAT YOU CAN DO FOR YOUR COUNTRY"
-word = "Country"
+word = "country"
 
 def task1(sentence, word):
     s = sentence.lower().split()
     positions = []
+    found = False
     for i in range(0, len(s)):
         if word.lower() == s[i]:
+            found = True
             positions.append(i+1)
-    return positions
+       
+    return positions if found else "Word not found"
 
 print(task1(sentence, word))
